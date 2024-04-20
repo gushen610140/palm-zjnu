e18577271dbe72eb8153d034c123ddb06013887d
gushen <w56038009@outlook.com>
2024-04-20 16:07:56 +0800

    ✨ feat: pinia持久化存储

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createSSRApp } from "vue";
+imoprt pinia from "./stores";
 import App from "./App.vue";
+
 export function createApp() {
   const app = createSSRApp(App);
+
+  app.use(pinia);
+
   return {
     app,
   };

07cdd7dedc37f5d1a42fa3c6ed17562cf45c4262
gushen <w56038009@outlook.com>
2024-06-29 17:24:35 +0800

    ✨ feat: 添加Moment类型

---
+++ src/types/Comment.ts
@@ -0,0 +1,16 @@
+export interface Comment {
+
+  id: string;
+
+  content: string;
+
+  date: string;
+
+  likes: number;
+
+  userAvator: string;
+
+  userId: string;
+
+  userName: string;
+}
---
+++ src/types/Moment.ts
@@ -0,0 +1,22 @@
+import { Comment } from './Comment.ts';
+
+export interface Moment {
+
+  comments: Comment[] | null;
+
+  content: null | string;
+
+  date: string;
+
+  id: string;
+
+  images: string[] | null;
+
+  likes: number;
+
+  userAvatar: string;
+
+  userId: string;
+
+  userName: string;
+}

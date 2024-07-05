1a9d5544f615024497270a13c81cf3167c9dd58e
gushen <w56038009@outlook.com>
2024-07-05 15:00:10 +0800

    ✨ feat: 优化了请求封装

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,17 +1,15 @@
 // 请求基地址
 const baseURL = 'http://127.0.0.1:8080'
 
-// 拦截器配置
+// 拦截器
 const httpInterceptor = {
-  // 拦截前触发
   invoke(options: UniApp.RequestOptions) {
-    // 1. 非 http 开头需拼接地址
+    // 非 http 开头拼接地址
     if (!options.url.startsWith('http')) {
       options.url = baseURL + options.url
     }
-    // 2. 请求超时
     options.timeout = 10000
-    // 3. 添加小程序端请求头标识
+    // 添加小程序端请求头标识
     options.header = {
       'source-client': 'miniapp',
       ...options.header,
@@ -27,55 +25,34 @@ const httpInterceptor = {
 
 // 拦截 request 请求
 uni.addInterceptor('request', httpInterceptor)
-// 拦截 uploadFile 文件上传
-uni.addInterceptor('uploadFile', httpInterceptor)
+
+import type { Result } from "@/types/Result"
 
 /**
- * 请求函数
- * @param  UniApp.RequestOptions
- * @returns Promise
- *  1. 返回 Promise 对象，用于处理返回值类型
- *  2. 获取数据成功
- *    2.1 提取核心数据 res.data
- *    2.2 添加类型，支持泛型
- *  3. 获取数据失败
- *    3.1 401错误  -> 清理用户信息，跳转到登录页
- *    3.2 其他错误 -> 根据后端错误信息轻提示
- *    3.3 网络错误 -> 提示用户换网络
+ * 
+ * @param options uni.request的请求参数
+ * @returns Promise对象 已经封装为Result类型
+ * 
+ * 泛型需要传入Result中返回的data的数据类型
+ * 
  */
-type Data<T> = {
-  code: string
-  msg: string
-  result: T
-}
-// 2.2 添加类型，支持泛型
 export const http = <T>(options: UniApp.RequestOptions) => {
-  // 1. 返回 Promise 对象
-  return new Promise<Data<T>>((resolve, reject) => {
+  return new Promise<Result<T>>((resolve, reject) => {
     uni.request({
       ...options,
-      // 响应成功
       success(res) {
-        // 状态码 2xx，参考 axios 的设计
         if (res.statusCode >= 200 && res.statusCode < 300) {
-          // 2.1 提取核心数据 res.data
-          resolve(res.data as Data<T>)
-        } else if (res.statusCode === 401) {
-          // 401错误  -> 清理用户信息，跳转到登录页
-          // const memberStore = useMemberStore()
-          // memberStore.clearProfile()
-          // uni.navigateTo({ url: '/pages/login/index' })
-          // reject(res)
+          // res是uni封装的返回类型，直接提取data是服务器定义的返回类型Result，as类型断言成前端定义的类型Result
+          resolve(res.data as Result<T>)
         } else {
-          // 其他错误 -> 根据后端错误信息轻提示
           uni.showToast({
             icon: 'none',
-            title: (res.data as Data<T>).msg || '请求错误',
+            title: (res.data as Result<T>).message || '请求错误',
           })
           reject(res)
         }
       },
-      // 响应失败
+      // 后端服务器无响应
       fail(err) {
         uni.showToast({
           icon: 'none',

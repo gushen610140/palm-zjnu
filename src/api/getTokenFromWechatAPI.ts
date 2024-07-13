2efa5dc80b23a42ba54b208534534a17a2311326
gushen610140 <w56038009@outlook.com>
2024-07-13 12:43:17 +0800

    ✨ feat: 更加安全的获取方式

--- src/api/getTokenFromWechatAPI.ts
+++ src/api/getTokenFromWechatAPI.ts
@@ -2,12 +2,17 @@ import type { Token } from "@/types/Token";
 import { http } from "@/utils/http";
 
 export const getTokenFromWechatAPI = async (code: string) => {
+  const res = await http<string>({
+    url: "/api/secret",
+    method: "GET",
+  });
+  const [appid, secret] = res.data.split(",")
   return http<Token>({
     url: "https://api.weixin.qq.com/sns/jscode2session",
     method: "GET",
     data: {
-      appid: "wx622113ff58e2b46b",
-      secret: "360464608fc569e5867a35e4cf5bb309",
+      appid,
+      secret,
       js_code: code,
       grant_type: "authorization_code",
     },
